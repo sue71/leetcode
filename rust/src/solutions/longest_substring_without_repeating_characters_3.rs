8b99f45b811213751795a8f9fde4391d82888feb
sue71 <s.masaki07@gmail.com>
2023-12-20 00:59:48 +0900

    solve 3

---
+++ rust/src/solutions/longest_substring_without_repeating_characters_3.rs
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=3 lang=rust
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+
+// @lc code=start
+
+use std::{collections::HashMap};
+
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let cs: Vec<char> = s.chars().collect();
+        let mut cursor = 0;
+        let mut len = 0;
+        let mut res = 0;
+        let mut map = HashMap::new();
+
+        for (i, c) in cs.iter().enumerate() {
+            if map.contains_key(c) && map[c] >= cursor {
+                println!("contains {} at {}", c, map[c]);
+                cursor = map[c] + 1;
+                len = i - map[c];
+                map.insert(c, i);
+                continue;
+            } else {
+                map.insert(c, i);
+                len += 1;
+            }
+            res = std::cmp::max(res, len);
+        }
+        res as i32
+    }
+}
+// @lc code=end
+
--- rust/src/solutions/mod.rs
+++ rust/src/solutions/mod.rs
@@ -1,3 +1,4 @@
 mod two_sum_1; 
 mod add_two_numbers_2;
 mod number_of_islands_200;
+mod longest_substring_without_repeating_characters_3;
