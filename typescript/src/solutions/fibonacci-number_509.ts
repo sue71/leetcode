25e0c8effe1dbeb74aa86a7233fe4c5f8f27b61c
sue71 <s.masaki07@gmail.com>
2025-09-22 14:19:43 +0900

    solve 509

---
+++ typescript/src/solutions/fibonacci-number_509.ts
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=509 lang=typescript
+ *
+ * [509] Fibonacci Number
+ */
+
+// @lc code=start
+function fib(n: number): number {
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+// @lc code=end
